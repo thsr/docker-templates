792d1b66cd36998ab597c8d89942c663be843373
thsr <437808@gmail.com>
2019-07-04 16:04:11 +0200

    working python-flask-postgres

--- python-flask-postgres/src/app.py
+++ python-flask-postgres/src/app.py
@@ -1,25 +1,32 @@
 from flask import Flask, jsonify
-from py2neo import Graph
+import psycopg2
 
 app = Flask(__name__)
 
 conn = psycopg2.connect(host="postgres", user="user", password="pass", database="db")
 
-@app.route('/initdb')
-def initdb():
+@app.route('/createtable')
+def createtable():
     with conn:
         with conn.cursor() as cur:
-            cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-            cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def", ))
-            return jsonify("ok")
+            cur.execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);")
+    return jsonify("ok")
 
-@app.route('/query')
-def query():
+@app.route('/insert')
+def insert():
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (999, "foo", ))
+            cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (747, "bar", ))
+    return jsonify("ok")
+
+@app.route('/select')
+def select():
     with conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM test;")
             res = cur.fetchall()
-            return jsonify(res)
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
